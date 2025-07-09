761585475f0c3b94796deb8599bef6c5aacc96e9
bp3456 <bharathp3456@gmail.com>
2025-07-09 11:00:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,16 @@ from langchain.chains import RetrievalQAWithSourcesChain, LLMChain
 # title
 st.set_page_config(page_title="Gen AI News Research", layout="wide")
 
-# NLTK setup
+# NLTK setup 
 nltk_data_dir = os.path.expanduser('~/nltk_data')
 os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.append(nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_dir)
+
 
 #Gemini API key
 os.environ["GOOGLE_API_KEY"] = st.secrets["api_key"]
